7e68655a6a154bfef99602f97e808aae335cc6d1
simonjupp123 <101952109+simonjupp123@users.noreply.github.com>
2024-08-28 21:11:21 -0400

    premerge

--- my_website/src/pages/Home.js
+++ my_website/src/pages/Home.js
@@ -1,11 +1,26 @@
 // src/pages/Home.js
 import React from 'react';
+import Typical from 'react-typical';
 import myImage from '..//images/IMG_0082.JPG';
 
 function Home() {
   return (
     <div>
       <h1>Welcome to My Portfolio</h1>
+      <h2>
+                I am a{' '}
+                <Typical
+                    steps={[
+                        'Software Engineer.', 1000,
+                        'Web Developer.', 1000,
+                        'Tech Enthusiast.', 1000,
+                        'Problem Solver.', 1000,
+                        'Lifelong Learner.', 1000
+                    ]}
+                    loop={Infinity}
+                    wrapper="span"
+                />
+            </h2>
       <section className="home-content">
         <img 
           src={myImage} // Replace with your own image URL or use local image
